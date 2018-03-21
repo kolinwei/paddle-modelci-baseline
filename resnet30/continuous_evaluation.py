08c803759ec2ab6998f497139269a609e8f4062d
Yan Chunwei <yanchunwei@outlook.com>
2018-03-21 13:46:07 +0800

    Update continuous_evaluation.py

--- resnet30/continuous_evaluation.py
+++ resnet30/continuous_evaluation.py
@@ -4,7 +4,7 @@ sys.path.append(os.environ['modelci_root'])
 from core import CostFactor, DurationFactor
 
 train_cost_factor = CostFactor('train_cost', 0.15)
-train_duration_factor = CostFactor('train_duration', 0.15)
+train_duration_factor = DurationFactor('train_duration', 0.15)
 
 tracking_factors = [
     train_cost_factor,
